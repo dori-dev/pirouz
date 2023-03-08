9474a4a874a0fd76e7e4fa348b0290c07303f24b
dori-dev <mr.dori.dev@gmail.com>
2023-03-08 13:29:40 +0330

    add class view

--- pirouz.py
+++ pirouz.py
@@ -1,6 +1,7 @@
+import inspect
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
-from werkzeug.exceptions import HTTPException, NotFound
+from werkzeug.exceptions import HTTPException, NotFound, MethodNotAllowed
 
 
 class TextResponse(Response):
@@ -25,19 +26,51 @@ class API():
         try:
             endpoint, kwargs = adapter.match()
             handler = self.routes[endpoint]
+            if inspect.isclass(handler):
+                method = request.method
+                handler = getattr(handler(), method.lower(), None)
+                if handler is None:
+                    return self.method_not_exist(request, method, endpoint)
             return handler(request, **kwargs)
         except NotFound:
             return self.error_404(request)
+        except MethodNotAllowed:
+            return self.error_405(request)
         except HTTPException as error:
             return error
 
     def error_404(self, request):
         return TextResponse('Page Not Found', status=404)
 
-    def route(self, path):
+    def error_405(self, request):
+        return TextResponse(
+            f'Method `{request.method}` is not allowed!',
+            status=405,
+        )
+
+    def method_not_exist(self, request, method, view_name):
+        return TextResponse(
+            "Server Error:\n"
+            f"The `{view_name}` view does not support"
+            f" the `{method}` method.",
+            status=500,
+        )
+
+    def check_route_exists(self, path, endpoint):
+        if endpoint in self.routes:
+            return True
+        for rule in self.url_rules.iter_rules():
+            if rule.rule == path:
+                return True
+        return False
+
+    def route(self, path, methods=None):
         def wrapper(handler):
             endpoint = handler.__name__
-            rule = Rule(path, endpoint=endpoint)
+            assert not self.check_route_exists(path, endpoint), (
+                "Path already exists!"
+            )
+            rule = Rule(path, endpoint=endpoint, methods=methods)
             self.routes[endpoint] = handler
             self.url_rules.add(rule)
             return handler
--- sample.py
+++ sample.py
@@ -8,6 +8,15 @@ def index(request):
     return TextResponse("Index Page.")
 
 
+@app.route("/detail/", methods=["GET", "POST"])
+class DetailView():
+    def get(self, request):
+        return TextResponse("Detail view with GET method.")
+
+    def post(self, request):
+        return TextResponse("Detail view with POST method.")
+
+
 @app.route('/dashboard/')
 def dashboard(request):
     return TextResponse("Dashboard Page")
@@ -16,3 +25,8 @@ def dashboard(request):
 @app.route('/user/<string:username>/')
 def user(request, username):
     return TextResponse(f"Hello, {username}")
+
+
+@app.route('/movie/<int:id>/')
+def movie(request, id):
+    return TextResponse(f"Loading the movie with id {id}")
