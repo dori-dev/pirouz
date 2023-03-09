05696c08c5e3cd8bf527e38804bfd8eb30660ad6
dori-dev <mr.dori.dev@gmail.com>
2023-03-09 21:34:51 +0330

    use orm in web framework

--- pirouz/__init__.py
+++ pirouz/__init__.py
@@ -1,3 +1,6 @@
 from .wsgi import App
 from .response import TextResponse, Render
 from .middleware import BaseMiddleware
+from dori_orm import DB, ResultConfig
+from dori_orm.operators import AND, OR, NOT
+from dori_orm import columns
