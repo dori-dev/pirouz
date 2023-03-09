dd63c8a334769b8bca33251e9343ddeae021caa9
dori-dev <mr.dori.dev@gmail.com>
2023-03-09 21:52:21 +0330

    add requirements

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@
 python3 setup.py sdist
 python -m twine upload dist/*
 """
-import os
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as f:
-    requires = f.read().splitlines()
-    print(requires)
-
 setuptools.setup(
     name="pirouz",
-    version="2.1.2",
+    version="2.2.3",
     author="Mohammad Dori",
     author_email="mr.dori.dev@gmail.com",
     description="A web framework built with Python.",
@@ -27,6 +22,13 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    install_requires=requires,
+    install_requires=[
+        "gunicorn==20.1.0",
+        "Jinja2==3.1.2",
+        "Werkzeug==2.2.3",
+        "pytest==7.2.2",
+        "requests==2.28.2",
+        "requests-wsgi-adapter==0.4.1",
+    ],
     python_requires=">=3.6",
 )
