8e72175e4a2e98f8a5417242bda02637ee416339
dori-dev <mr.dori.dev@gmail.com>
2023-03-09 21:54:58 +0330

    fixed problem in orm

--- pirouz/__init__.py
+++ pirouz/__init__.py
@@ -1,6 +1,6 @@
 from .wsgi import App
 from .response import TextResponse, Render
 from .middleware import BaseMiddleware
-from orm import DB, ResultConfig
-from orm.operators import AND, OR, NOT
-from orm import columns
+from .orm import DB, ResultConfig
+from .orm.operators import AND, OR, NOT
+from .orm import columns
--- pirouz/orm/db.py
+++ pirouz/orm/db.py
@@ -3,8 +3,8 @@ import os
 import sqlite3
 import inspect
 from typing import Dict, List, NamedTuple, Tuple, Union
-from dori_orm.operators import OPERATORS
-from dori_orm.columns import ForeignKey
+from .operators import OPERATORS
+from .columns import ForeignKey
 
 
 class GenerateTableName:
