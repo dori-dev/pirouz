2660af7dd619092a561a847ece8468de789b0389
dori-dev <mr.dori.dev@gmail.com>
2023-03-09 21:24:49 +0330

    add pirouz to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import setup, find_packages
-setup(
-    name='pirouz',
-    packages=find_packages(),
+"""
+python3 setup.py sdist
+python -m twine upload dist/*
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pirouz",
+    version="1.0.0",
+    author="Mohammad Dori",
+    author_email="mr.dori.dev@gmail.com",
+    description="A web framework built with Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dori-dev/pirouz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
 )
