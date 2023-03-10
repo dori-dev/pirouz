9e99fba9f76ea8921df15a047f060d20d0b455cb
dori-dev <mr.dori.dev@gmail.com>
2023-03-10 15:29:21 +0330

    add redirect

--- pirouz/response.py
+++ pirouz/response.py
@@ -1,6 +1,7 @@
 import os
 
 from werkzeug.wrappers import Response
+from werkzeug.routing import RequestRedirect
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -23,3 +24,8 @@ class Render(Response):
         )
         text = self.templates_env.get_template(template_name).render(**context)
         super().__init__(text, content_type='text/html', **kwargs)
+
+
+def redirect(url: str) -> Response:
+    response = RequestRedirect(url)
+    return response.get_response()
