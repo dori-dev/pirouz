004706ed5d2bf0715b70e34ccd696a7a53568eb9
dori-dev <mr.dori.dev@gmail.com>
2023-03-10 15:30:45 +0330

    level up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pirouz",
-    version="2.2.4",
+    version="2.3.1",
     author="Mohammad Dori",
     author_email="mr.dori.dev@gmail.com",
     description="A web framework built with Python.",
